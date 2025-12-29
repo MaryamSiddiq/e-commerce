50cbc62551a7a813cf6b2423e8d9bb644095a3c2
Maryam Siddique <maryamsiddique449@example.com>
2025-12-29 21:53:41 +0500

    updated

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, 'Please provide a username'],
+    required: [true, 'Please provide a username '],
     unique: true,
     trim: true,
     minlength: [3, 'Username must be at least 3 characters'],
